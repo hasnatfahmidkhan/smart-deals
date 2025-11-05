026229784d27095f6a6ee4f1ef72149542676ad0
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-11-06 01:12:55 +0600

    added scroll effect

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../Components/Navbar/Navbar";
+import { useEffect } from "react";
 
 const Root = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
